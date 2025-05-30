2c25dd19f97dba8c1fc57630b1d403e8a6f72419
Hariharan794 <harihp13@gmail.com>
2025-05-30 16:28:57 +0530

    Example

---
+++ src/org/datatypes/datas.java
@@ -0,0 +1,26 @@
+package org.datatypes;
+
+public class datas {
+	public static void main(String[] args) {
+		int age =10;
+		System.out.println(age);
+		
+		
+		boolean married = false;
+		System.out.println(married);
+		
+		long empl =741258963l;
+		System.out.println(empl);
+		
+		double longtitude=12.52845;
+		System.out.println(longtitude);
+		
+		char aish='h';
+		System.out.println(aish);
+		
+		byte ghj = 15;
+		System.out.println(ghj);
+		
+	}
+
+}
---
+++ src/org/datatypes/datatype_class.java
@@ -0,0 +1,30 @@
+package org.datatypes;
+
+public class datatype_class {
+	public static void main(String[] args) {
+		System.out.println("Employee details:");
+		
+		byte data = 20;
+		System.out.println("employe data:"+data);
+		
+		int age = 21;
+		System.out.println("employee age:"+age);
+		
+		short serialno =1330;
+		System.out.println("employee serial no:"+serialno);
+		
+		long phone = 9043128021l;
+		System.out.println("employee no:"+phone);
+		
+		double lattitude = 1285.85285;
+		System.out.println("employee current lattitude location:"+lattitude);
+		
+		float perctage =10.54f;
+		System.out.println("employee perctage:"+perctage);
+		
+		char gender ='m';
+		System.out.println("employee gender:"+gender);
+	}
+
+
+}
